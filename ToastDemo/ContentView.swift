b5a09e609c9a2e7b63067a11537c508a446d1832
Peter Murín <peter.murin@codium.sk>
2023-11-22 21:45:42 +0100

    Initial Commit

---
+++ ToastDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ToastDemo
+//
+//  Created by Peter Murín on 22/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ToastDemo/ToastDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToastDemoApp.swift
+//  ToastDemo
+//
+//  Created by Peter Murín on 22/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct ToastDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
